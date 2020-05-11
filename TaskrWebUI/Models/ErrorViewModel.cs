24d5f5314ef08623ceb0eee4352fe9b3663ef2f1
Jeff Adams <atomicxistence@gmail.com>
2020-05-10 20:56:16 -0400

    Iniatilize a ASP.NET project for the webui

---
+++ TaskrWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskrWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
