fac05e8b71542b9171c24d21ff14b9d48972074a
Jeff Adams <atomicxistence@gmail.com>
2020-05-12 23:02:17 -0400

    Add sqlite and dapper packages | Start creating db transaction class

--- TaskrLibrary/FileIO/SqliteTransaction.cs
+++ TaskrLibrary/FileIO/SqliteTransaction.cs
@@ -1,13 +1,23 @@
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using Microsoft.Data.Sqlite;
 using TaskrLibrary.Models;
 
 namespace TaskrLibrary.FileIO
 {
     public class SqliteTransaction : IFileTransaction
     {
+        private readonly string connectionString = "Data Source=Taskr.db;Version=3";
+
         public List<Task> Load()
         {
-            throw new System.NotImplementedException();
+            using (IDbConnection db = new SqliteConnection(connectionString))
+            {
+                var tasks = db.Query<Task>("SELECT * FROM Task");
+                return tasks.ToList();
+            }
         }
 
         public Task InsertTask(Task task)
